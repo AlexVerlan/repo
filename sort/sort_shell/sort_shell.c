37cc4eef61795c2c52c5083138dae3a8cc9cb7d4
AlexVerlan <alexverlanp@gmail.com>
2023-02-11 14:58:56 +0300

    реализация сортировки шела C:\Users\Alex_p\Desktop\repo\sort\sort_shell

--- sort/sort_shell/sort_shell.c
+++ sort/sort_shell/sort_shell.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+void swap(int* a,int*b){
+		int buf;
+		buf=*a;
+		*a=*b;
+		*b=buf;
+	}
 
 void sort_shell (int* arr,int size){
-		
+		int buf=size/2;
+		while(buf>=1){
+			for(int i=0;i<buf;++i){
+				for(int x=(i+buf);x<size;x+=buf){
+					if(arr[x]<arr[x-buf]){
+						swap(arr+x,arr+(x-buf));
+					}
+				}
+				
+			}
+			buf/=2;			
+			
+		}
 	
 	}
+
+int main(int* argc, char** argv){
+	int arr[20];
+	for(int i=0;i<20;++i){
+		arr[i]=rand()%30;
+	}
+	for(int i=0;i<20;++i){
+		printf("[%d]=%d ",i,arr[i]);
+	}
+	printf("\n_______\n");
+	sort_shell((int*)arr,20);
+	for(int i=0;i<20;++i){
+		printf("arr[%d] = %d ",i,arr[i]);
+	}
+	return 0;
+}
